c1cb6453b7d3bc975c96cecf5e259e07480ee3ce
chiangyiyang <chiangyiyang@gmail.com>
2023-12-12 15:52:30 +0800

    get posts with json format

--- app.js
+++ app.js
@@ -2,10 +2,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let posts = [
+    {
+        "id": 1,
+        "title": "json-server",
+        "author": "typicode"
+    },
+    {
+        "id": 2,
+        "title": "json-server is easy!!",
+        "author": "typicode"
+    },
+    {
+        "id": 3,
+        "title": "json-server is fun!!",
+        "author": "typicode"
+    }
+]
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    //   res.send('Hello World!')
+    res.json({ message: "Hello World!" })
+})
+
+app.get('/posts', (req, res) => {
+    res.json(posts)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
